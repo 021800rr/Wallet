0864dd5e685fbf7d5d9bbdca84951d5e3a6ae273
Ryszard Ratajczak <ryszard.ratajczak@onet.pl>
2021-11-16 06:26:33 +0100

    EUR, CHF

--- src/Service/BalanceUpdater/BackupBalanceUpdater.php
+++ src/Service/BalanceUpdater/BackupBalanceUpdater.php
@@ -12,16 +12,16 @@ class BackupBalanceUpdater extends AbstractBalanceUpdater implements BalanceUpda
         if (Backup::INAPPLICABLE === $transaction->getInterest()) {
             $transaction->setBalance($predecessor->getBalance() + $transaction->getAmount());
             $transaction = $this->setSubWallets($predecessor, $transaction);
-            $this->entityManager->persist($transaction);
-            $this->entityManager->flush();
         } elseif (Backup::NOT_PROCESSED === $transaction->getInterest()) {
             $transaction->setBalance($predecessor->getBalance() + $transaction->getBalance());
             $transaction->setRetiring($predecessor->getRetiring() + $transaction->getRetiring());
             $transaction->setHoliday($predecessor->getHoliday() + $transaction->getHoliday());
             $transaction->setInterest(Backup::DONE);
-            $this->entityManager->persist($transaction);
-            $this->entityManager->flush();
+        } elseif (Backup::DONE === $transaction->getInterest()) {
+            $transaction->setBalance($predecessor->getBalance() + $transaction->getAmount());
         }
+        $this->entityManager->persist($transaction);
+        $this->entityManager->flush();
 
         if (count($successors)) {
             $predecessor = $transaction;
