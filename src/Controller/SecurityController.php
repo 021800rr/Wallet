516abea810e93a9c1818e093b3b5ba17ad2d01d8
Ryszard Ratajczak <ryszard.ratajczak@onet.pl>
2022-05-29 11:26:00 +0200

    5.3 to 5.4 security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -10,16 +10,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="app_login")
-     */
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
-    }
+//    /**
+//     * @Route("/login", name="app_login")
+//     */
+//    public function login(AuthenticationUtils $authenticationUtils): Response
+//    {
+//        $error = $authenticationUtils->getLastAuthenticationError();
+//        $lastUsername = $authenticationUtils->getLastUsername();
+//
+//        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+//    }
 
     /**
      * @Route("/logout", name="app_logout")
