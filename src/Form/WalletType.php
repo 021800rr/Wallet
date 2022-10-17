142c2c440ec3548162060aca600237fc88821d40
Ryszard Ratajczak <ryszard.ratajczak@nocowanie.pl>
2022-10-17 07:02:08 +0200

    id from the database is visible on the list of operations

--- src/Form/WalletType.php
+++ src/Form/WalletType.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -25,7 +26,7 @@ class WalletType extends AbstractType
                 'currency' => 'PLN',
                 'attr' => [
                     'autofocus' => true,
-                    'required' => true
+                    'required' => true,
                 ]
             ])
             ->add('contractor', EntityType::class, [
