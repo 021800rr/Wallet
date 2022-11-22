36fad4641c4f044476ed48d4865aa4049b05ca65
Ryszard Ratajczak <ryszard.ratajczak@nocowanie.pl>
2022-11-22 13:12:07 +0100

    fee tests

--- tests/Controller/FeeControllerTest.php
+++ tests/Controller/FeeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\ContractorRepository;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -12,6 +13,85 @@ class FeeControllerTest extends WebTestCase
     private KernelBrowser $client;
 
     public function testIndex(): void
+    {
+        $this->client->request('GET', '/en/fee');
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount1', '-19.99');
+
+        $this->assertSelectorTextContains('td#fee_contractor2', 'Netflix');
+        $this->assertSelectorTextContains('td#fee_amount2', '-52');
+    }
+
+    public function testNew(): void
+    {
+        $this->client->request('GET', '/en/fee');
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount1', '-19.99');
+
+        $this->assertSelectorTextContains('td#fee_contractor2', 'Netflix');
+        $this->assertSelectorTextContains('td#fee_amount2', '-52');
+
+        $this->client->request('GET', '/en/fee/new');
+        $this->client->submitForm('Save', [
+            'fee[date]' => '20',
+            'fee[amount]' => '-21',
+            'fee[contractor]' => $this->contractor->getId(),
+        ]);
+
+        $this->assertSelectorTextContains('td#fee_contractor1', ContractorRepository::INTERNAL_TRANSFER);
+        $this->assertSelectorTextContains('td#fee_amount1', '-21');
+
+        $this->assertSelectorTextContains('td#fee_contractor2', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount2', '-19.99');
+    }
+
+    public function testEdit(): void
+    {
+        $crawler = $this->client->request('GET', '/en/fee');
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount1', '-19.99');
+
+        $crawler = $this->client->click(
+            $crawler->filter('a#fee_edit1')->link()
+        );
+
+        $form = $crawler->selectButton('fee_save')->form();
+        $values = $form->getValues();
+
+        $this->assertSame('-19.99', $values["fee[amount]"]);
+        $form['fee[amount]']->setValue(-19.9);
+        $this->assertSame('4', $values["fee[date]"]);
+        $form['fee[date]']->setValue(14);
+
+        $this->client->submit($form);
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount1', '-19.9');
+        $this->assertSelectorTextContains('td#fee_date1', '14');
+    }
+
+    public function testDelete(): void
+    {
+        $crawler = $this->client->request('GET', '/en/fee');
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Spotify');
+        $this->assertSelectorTextContains('td#fee_amount1', '-19.99');
+
+        $this->assertSelectorTextContains('td#fee_contractor2', 'Netflix');
+        $this->assertSelectorTextContains('td#fee_amount2', '-52');
+
+        $this->client->submit(
+            $crawler->filter('form#fee_delete1')->form()
+        );
+
+        $this->assertSelectorTextContains('td#fee_contractor1', 'Netflix');
+        $this->assertSelectorTextContains('td#fee_amount1', '-52');
+    }
+
+    public function testInsert(): void
     {
         $this->client->request('GET', '/en/wallet');
 
--- tests/Controller/Setup.php
+++ tests/Controller/Setup.php
@@ -2,10 +2,14 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Contractor;
+use App\Repository\ContractorRepository;
 use App\Repository\UserRepository;
 
 trait Setup
 {
+    protected Contractor $contractor;
+
     protected function setUp(): void
     {
         $this->client = static::createClient();
@@ -13,6 +17,9 @@ trait Setup
         $testUser = $userRepository->findOneByUsername('rr');
         $this->client->loginUser($testUser);
         $this->client->followRedirects();
+
+        $contractorRepository = static::getContainer()->get(ContractorRepository::class);
+        $this->contractor = $contractorRepository->getInternalTransferOwner();
 
         parent::setUp();
     }
