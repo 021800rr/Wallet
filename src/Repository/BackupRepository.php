5e97c06661c5385734518448fb87beb06d96baba
Ryszard Ratajczak <ryszard.ratajczak@protonmail.com>
2022-12-07 15:50:00 +0100

    temporary hack for backups

--- src/Repository/BackupRepository.php
+++ src/Repository/BackupRepository.php
@@ -26,7 +26,7 @@ class BackupRepository extends ServiceEntityRepository implements BackupReposito
     {
         return $this->createQueryBuilder('p')
             ->select('p.yearMonth', 'SUM(p.amount) as sum_of_amount')
-            ->where('p.amount > 99')
+//            ->where('p.amount > 99')
             ->groupBy('p.yearMonth')
             ->orderBy('p.yearMonth', 'DESC')
             ->setMaxResults(self::PAYMENTS_BY_MONTH_YEARS)
