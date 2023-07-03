e446552d8dc7f46c963950c8a4614def84d3ba37
Ryszard Ratajczak <ryszard.ratajczak@protonmail.com>
2023-07-03 08:41:32 +0200

    euro

--- tests/Controller/BackupControllerTest.php
+++ tests/Controller/BackupControllerTest.php
@@ -77,7 +77,6 @@ class BackupControllerTest extends WebTestCase
         $this->assertSelectorTextContains('td#backup_sum_of_amounts1', '300');
         $this->assertSelectorTextContains('td#expected', '300');
         $this->assertSelectorTextContains('td#chfBalance', '70.07');
-        $this->assertSelectorTextContains('td#eurBalance', '70.07');
         $this->assertSelectorTextContains('td#plnBalance', '170');
         $this->assertSelectorTextContains('td#holiday', '300');
         $this->assertSelectorTextContains('td#retiring', '300');
