4ff2f6b4e0f1fc390ccace36892bda1195cdc724
Ryszard Ratajczak <ryszard.ratajczak@protonmail.com>
2024-07-15 07:55:40 +0200

    rebuild

--- rebuild.sh
+++ rebuild.sh
@@ -1,3 +1,3 @@
 docker compose --env-file .env.prod.local down --remove-orphans && \
-docker compose --env-file .env.prod.local build && \
+docker compose --env-file .env.prod.local build --no-cache && \
 docker compose --env-file .env.prod.local up -d
